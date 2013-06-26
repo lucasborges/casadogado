250290c9ee3314800144c24f430b8d029d8eb3c2
lucasborges <lucas.teixeiraborges@gmail.com>
2013-06-26 14:12:21 -0300

    Update painel_config.php

--- Connections/painel_config.php
+++ Connections/painel_config.php
@@ -2,8 +2,8 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_painel_config = "casadogado.com.br";
-$database_painel_config = "casadogado";
+$hostname_painel_config = " mysql.casadogado.com.br";
+$database_painel_config = "casadogado_www";
 $username_painel_config = "casadogado";
 $password_painel_config = "1q2w3e4r";
 $painel_config = mysql_pconnect($hostname_painel_config, $username_painel_config, $password_painel_config) or trigger_error(mysql_error(),E_USER_ERROR); 
