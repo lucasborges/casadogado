21c4480519a6c48fd88def4aa5cf99c920ab7f68
lucasborges <lucas.teixeiraborges@gmail.com>
2013-06-27 17:00:53 -0300

    If

--- header_index.php
+++ header_index.php
@@ -1,95 +1,7 @@
 
-
-<?php 	
-	require_once('Connections/painel_config.php');
-?>
-
-<?php
-
-if (!function_exists("GetSQLValueString")) {
-function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
-{
-  if (PHP_VERSION < 6) {
-    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
-  }
-
-  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
-
-  switch ($theType) {
-    case "text":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;    
-    case "long":
-    case "int":
-      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
-      break;
-    case "double":
-      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
-      break;
-    case "date":
-      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
-      break;
-    case "defined":
-      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
-      break;
-  }
-  return $theValue;
-}
-}
-?>
-
-<?php
-// *** Validate request to login to this site.
-if(isset($_POST['logar']) && $_POST['logar'] == 'Logar'){ // Inicio IF
-		if (!isset($_SESSION)) {
-		  session_start();
-		}
-		
-		$loginFormAction = $_SERVER['PHP_SELF'];
-		if (isset($_GET['accesscheck'])) {
-		  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
-		}
-		
-		if (isset($_POST['email'])) {
-		  $loginUsername=$_POST['email'];
-		  $password=md5($_POST['senha']);
-		  $MM_fldUserAuthorization = "usuarioNivel";
-		  $MM_redirectLoginSuccess = "admin/painel.php";
-		  $MM_redirectLoginFailed = "admin/erro.php";
-		  $MM_redirecttoReferrer = false;
-		  mysql_select_db($database_painel_config, $painel_config);
-			
-		  $LoginRS__query=sprintf("SELECT email, senha, usuarioNivel FROM portal_cliente WHERE email=%s AND senha=%s",
-		  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
-		   
-		  $LoginRS = mysql_query($LoginRS__query, $painel_config) or die(mysql_error());
-		  $loginFoundUser = mysql_num_rows($LoginRS);
-		  if ($loginFoundUser) {
-			
-			$loginStrGroup  = mysql_result($LoginRS,0,'usuarioNivel');
-			
-			if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
-			//declare two session variables and assign them
-			$_SESSION['MM_Username'] = $loginUsername;
-			$_SESSION['MM_UserGroup'] = $loginStrGroup;	      
-		
-			if (isset($_SESSION['PrevUrl']) && false) {
-			  $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
-			}
-			header("Location: " . $MM_redirectLoginSuccess );
-		  }
-		  else {
-			header("Location: ". $MM_redirectLoginFailed );
-		  }
-		}
-}
-?>
-
+<?php session_start();?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<?php 
-  session_start();
-?>
 <head>
 <?php include"Connections/config.php";?>
 <?php include"js/scripts.php";?>
@@ -190,3 +102,87 @@ if(isset($_POST['logar']) && $_POST['logar'] == 'Logar'){ // Inicio IF
         	</div>
           	<!--  FIM Header Navegar-->
         </div> <!-- Fim Div header -->
+
+
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+?>
+
+<?php
+
+	require_once('Connections/painel_config.php');
+
+// *** Validate request to login to this site.
+if(isset($_POST['logar']) && $_POST['logar'] == 'Logar'){ // Inicio IF
+		if (!isset($_SESSION)) {
+		  session_start();
+		}
+		
+		$loginFormAction = $_SERVER['PHP_SELF'];
+		if (isset($_GET['accesscheck'])) {
+		  $_SESSION['PrevUrl'] = $_GET['accesscheck'];
+		}
+		
+		if (isset($_POST['email'])) {
+		  $loginUsername=$_POST['email'];
+		  $password=md5($_POST['senha']);
+		  $MM_fldUserAuthorization = "usuarioNivel";
+		  $MM_redirectLoginSuccess = "admin/painel.php";
+		  $MM_redirectLoginFailed = "admin/erro.php";
+		  $MM_redirecttoReferrer = false;
+		  mysql_select_db($database_painel_config, $painel_config);
+			
+		  $LoginRS__query=sprintf("SELECT email, senha, usuarioNivel FROM portal_cliente WHERE email=%s AND senha=%s",
+		  GetSQLValueString($loginUsername, "text"), GetSQLValueString($password, "text")); 
+		   
+		  $LoginRS = mysql_query($LoginRS__query, $painel_config) or die(mysql_error());
+		  $loginFoundUser = mysql_num_rows($LoginRS);
+		  if ($loginFoundUser) {
+			
+			$loginStrGroup  = mysql_result($LoginRS,0,'usuarioNivel');
+			
+			if (PHP_VERSION >= 5.1) {session_regenerate_id(true);} else {session_regenerate_id();}
+			//declare two session variables and assign them
+			$_SESSION['MM_Username'] = $loginUsername;
+			$_SESSION['MM_UserGroup'] = $loginStrGroup;	      
+		
+			if (isset($_SESSION['PrevUrl']) && false) {
+			  $MM_redirectLoginSuccess = $_SESSION['PrevUrl'];	
+			}
+			header("Location: " . $MM_redirectLoginSuccess );
+		  }
+		  else {
+			header("Location: ". $MM_redirectLoginFailed );
+		  }
+		}
+}
+?>
