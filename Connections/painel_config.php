1b7af22f06ec725a487969486ea62d00e0a77cd5
lucasborges <lucas.teixeiraborges@gmail.com>
2013-06-28 17:15:14 -0300

    a

--- Connections/painel_config.php
+++ Connections/painel_config.php
@@ -4,4 +4,3 @@ $database_painel_config = "casadogado_www";
 $username_painel_config = "casadogado";
 $password_painel_config = "1q2w3e4r";
 $painel_config = mysql_pconnect($hostname_painel_config, $username_painel_config, $password_painel_config) or trigger_error(mysql_error(),E_USER_ERROR); 
-?>
