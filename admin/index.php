06117ecd87750810b05785cdc5fadff075156db0
lucasborges <lucas.teixeiraborges@gmail.com>
2013-07-05 09:42:16 -0300

    .

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,26 @@
-<?php session_start();?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>.: Portal do Gado :.</title>
+<link href="login_style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+  <div id="login">  <!-- Inicio login -->
+    	<img src="imagens/portaldogado_logo.png" alt=""/>
+    	<form name="login_painel"  action="<?php echo $loginFormAction; ?>" method="POST">
+        	<label><span> E-mail:</span> <input type="text" name="email"/> </label>
+            <label><span> Senha:  </span> <input type="password" name="senha"/> </label>
+            <p> <a href="recover.php">[Esqueci minha senha] </a></p>
+            <input type="submit" name="logar" value="Logar" class="btn" />
+        </form>
+    </div> <!-- Fecha login -->
+    
+    
+</body>
+</html>
 <?php require_once('../Connections/painel_config.php'); ?>
 <?php
 if (!function_exists("GetSQLValueString")) {
@@ -74,27 +96,4 @@ if (isset($_POST['email'])) {
   else {
     header("Location: ". $MM_redirectLoginFailed );
   }
-}?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>.: Portal do Gado :.</title>
-<link href="login_style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-
-  <div id="login">  <!-- Inicio login -->
-    	<img src="imagens/portaldogado_logo.png" alt=""/>
-    	<form name="login_painel"  action="<?php echo $loginFormAction; ?>" method="POST">
-        	<label><span> E-mail:</span> <input type="text" name="email"/> </label>
-            <label><span> Senha:  </span> <input type="password" name="senha"/> </label>
-            <p> <a href="recover.php">[Esqueci minha senha] </a></p>
-            <input type="submit" name="logar" value="Logar" class="btn" />
-        </form>
-    </div> <!-- Fecha login -->
-    
-    
-</body>
-</html>
+}
